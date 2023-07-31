d1298b1ed685009f00be612217a4634e982ec31b
Ji-Yeon-98 <cn0714@naver.com>
2023-07-31 10:48:57 +0900

    [level 0] Title: 배열 뒤집기, Time: 9.02 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer:IntArray = intArrayOf()
+
+        for (i in num_list.size-1 downTo 0){
+            answer = answer.plus(num_list[i])
+        }
+        return answer
+    }
+}
