b59987081b015853ee59d18fd4e4db3421112739
Ji-Yeon-98 <cn0714@naver.com>
2023-08-07 12:04:21 +0900

    [level 0] Title: 이진수 더하기, Time: 18.10 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.kt
@@ -0,0 +1,36 @@
+class Solution {
+    fun solution(bin1: String, bin2: String): String {
+        val maxLength = maxOf(bin1.length, bin2.length)
+        val c_bin1 = bin1.padStart(maxLength, '0').toCharArray()
+        val c_bin2 = bin2.padStart(maxLength, '0').toCharArray()
+
+        var result = mutableListOf<Int>()
+        var c = 0
+
+        for (i in c_bin1.size - 1 downTo 0) {
+            var a = Integer.parseInt(c_bin1[i].toString())
+            var b = Integer.parseInt(c_bin2[i].toString())
+
+            if (a + b + c == 0) {
+                result.add(0)
+                c = 0
+            } else if (a + b + c == 1) {
+                result.add(1)
+                c = 0
+            } else if (a + b + c == 2) {
+                result.add(0)
+                c = 1
+            } else if (a + b + c == 3) {
+                result.add(1)
+                c = 1
+            }
+
+            if(i == 0 && c ==1){
+                result.add(1)
+            }
+
+        }
+
+        return result.reversed().joinToString ("")
+    }
+}
