6313a789ceedc2c69ee3dfbc99bb18c0def85777
Ji-Yeon-98 <cn0714@naver.com>
2023-08-10 10:06:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 28.54 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.kt
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.kt
@@ -19,3 +19,24 @@ class Solution {
         
     }
 }
+
+
+
+// val sample = mapOf(
+//         "0" to "zero",
+//         "1" to "one",
+//         "2" to "two",
+//         "3" to "three",
+//         "4" to "four",
+//         "5" to "five",
+//         "6" to "six",
+//         "7" to "seven",
+//         "8" to "eight",
+//         "9" to "nine"
+//     )
+    
+//     var result = numbers
+    
+//     sample.forEach{ result = result.replace(it.value, it.key) }
+    
+//     return result.toLong()
