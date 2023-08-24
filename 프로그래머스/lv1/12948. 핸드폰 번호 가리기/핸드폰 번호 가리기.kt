35d157a24e4b655d92071919fe05b2e7cced0f44
Ji-Yeon-98 <cn0714@naver.com>
2023-08-24 09:49:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 13.61 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(phone_number: String): String {
+        var answer = ""
+
+        for ( i in 0 until phone_number.length){
+            if(i < phone_number.length-4){
+                answer = answer + "*"
+            }else{
+                answer = answer + phone_number[i]
+            }
+        }
+        
+        return answer
+    }
+}
