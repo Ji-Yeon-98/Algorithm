75f93774a5b2ab517e751a600e8710d7ebcf9acd
Ji-Yeon-98 <cn0714@naver.com>
2024-01-19 15:32:19 +0900

    [level 2] Title: 귤 고르기, Time: 81.97 ms, Memory: 98.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+class Solution {
+    fun solution(k: Int, tangerine: IntArray): Int {
+        
+        val valueList = tangerine.groupBy { it }.mapValues { it.value.size }.values.sortedDescending()
+
+        var answer = 0
+        var sum = 0
+
+        valueList.takeWhile { sum += it; answer++; sum < k }
+
+        return answer
+    }
+}
