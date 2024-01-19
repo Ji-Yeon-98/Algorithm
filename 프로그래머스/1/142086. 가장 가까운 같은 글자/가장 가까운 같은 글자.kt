e80bd37f92b09bb33706334110f576e3e44a4df0
Ji-Yeon-98 <cn0714@naver.com>
2024-01-19 17:55:56 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1280.38 ms, Memory: 361 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
@@ -16,3 +16,14 @@ class Solution {
         return answer.toIntArray()
     }
 }
+
+// class NearestSameLetter {
+//     fun solution(s: String): IntArray {
+//         val prevIndexMap = hashMapOf<Char, Int>()
+//         return s.mapIndexed { index, c ->
+//             val prevIndex = prevIndexMap[c]
+//             prevIndexMap[c] = index
+//             if (prevIndex == null) -1 else index - prevIndex
+//         }.toIntArray()
+//     }
+// }
